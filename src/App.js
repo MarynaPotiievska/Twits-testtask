fb7200818741a8f0c81816518a330035987b5b6c
MarynaPotiievska <101947859+MarynaPotiievska@users.noreply.github.com>
2023-06-17 20:37:49 +0300

    added react-router-dom and emotion libraries, created pages and added routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { lazy } from "react";
+import { Route, Routes } from "react-router-dom";
+
+const Home = lazy(() => import("pages/Home"));
+const Tweets = lazy(() => import("pages/Tweets"));
+
 const App = () => {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/tweets" element={<Tweets />}></Route>
+    </Routes>
+  );
 };
 
 export default App;
